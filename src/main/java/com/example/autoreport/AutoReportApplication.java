12210b3b231c0557240f36e05b4e28708452f2ba
kaori-seasons <yuluoxinsheng@gmail.com>
2025-06-27 17:08:34 +0800

    add scan configuration

--- src/main/java/com/example/autoreport/AutoReportApplication.java
+++ src/main/java/com/example/autoreport/AutoReportApplication.java
@@ -3,10 +3,12 @@ package com.example.autoreport;
 import org.springframework.ai.model.chat.client.autoconfigure.ChatClientAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {ChatClientAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.example.autoreport", "com.alibaba.cloud.ai"})
 @EnableAsync
 @EnableScheduling
 public class AutoReportApplication {
